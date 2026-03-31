930a3d1f8c295d7f36c64d49f5a75d68e461550e
jv marte <jvmarte20@gmail.com>
2026-04-01 05:32:27 +0800

    Fix CSP to allow inline event handlers

--- server/server.js
+++ server/server.js
@@ -22,8 +22,9 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc:  ["'self'", "'unsafe-inline'"],
-      styleSrc:   ["'self'", "'unsafe-inline'"],
+      scriptSrc:     ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
+      styleSrc:      ["'self'", "'unsafe-inline'"],
       imgSrc:     ["'self'", 'data:']
     }
   }
